6435c0f22daf2e1bac5677a9b6ba35dce91cda83
in choi <vcode@vcode-studio.com>
2023-07-11 18:37:26 +0900

    d

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ const crypto = require("crypto");
 function rn() {
   return crypto.randomBytes(16).toString("hex");
 }
-
 module.exports = {
   apps: [
     {
@@ -19,6 +18,13 @@ module.exports = {
         API_TOKEN_SALT: rn(),
         JWT_SECRET: rn(),
         ADMIN_JWT_SECRET: rn(),
+        // aws s3
+        AWS_ACCESS_KEY_ID: "AKIARJGLM3WL5PLEQ6MT",
+        AWS_ACCESS_SECRET: "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",
+        AWS_REGION: "ap-northeast-2",
+        AWS_ACL: "public-read",
+        AWS_SIGNED_URL_EXPIRES: 900,
+        AWS_BUCKET: "stravelte",
       },
     },
     {
@@ -29,7 +35,6 @@ module.exports = {
       restartDelay: 3000,
       env: {
         NODE_ENV: "development",
-        PORT: 3000,
       },
     },
   ],
