ce3948b1d8eec27a1c728603381b186fdeaced2d
Choiin <vcode@vcode-studio.com>
2023-07-11 16:35:02 +0900

    d

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
-  import Header from "./Header.svelte";
+  import "bootstrap-icons/font/bootstrap-icons.css";
+  import "pretendard/dist/web/variable/pretendardvariable.css";
+  import "../app.css";
 	import "./style.less";
+  import Header from "./Header.svelte";
 </script>
 
 <Header/>
+
 <slot/>
--- web/src/routes/Header.svelte
+++ web/src/routes/Header.svelte
@@ -1,3 +1,7 @@
-<div>
-	1
-</div>
+<header>
+	<div class="w-limit flex w-full gap-4 fixed left-0 top-0 p-4">
+		<div>1</div>
+		<div class="flex-auto text-center">2</div>
+		<div>2</div>
+	</div>
+</header>
