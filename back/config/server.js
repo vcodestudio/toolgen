04d05bd48a83f9372c53992b19cac10ed8d8eef3
in choi <vcode@vcode-studio.com>
2023-08-04 12:58:24 +0900

    d

--- back/config/server.js
+++ back/config/server.js
@@ -1,7 +1,7 @@
 const cronTasks = require("./cron-tasks");
 
 module.exports = ({ env }) => ({
-  url: env("PUBLIC_URL", "http://localhost:1337"),
+  // url: env("PUBLIC_URL", "http://localhost:1337"),
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   cron: {
