c870425dc66b0f1ddac39f5608a6eec82a925d3d
in choi <vcode@vcode-studio.com>
2023-08-10 16:30:54 +0900

    d

--- web/src/components/PageMarginTop.svelte
+++ web/src/components/PageMarginTop.svelte
@@ -1,4 +1,14 @@
 <script>
-    import {hHeight} from "$lib/store";
+  import { hHeight } from '$lib/store'
+  import { onMount } from 'svelte'
+
+  let height = 136
+  onMount(() => {
+    height = innerWidth < 1080 ? 56 : 136
+    addEventListener('resize', () => {
+      height = innerWidth < 1080 ? 56 : 136
+    })
+  })
 </script>
-<div class="w-full" style:height={$hHeight + "px"}></div>
+
+<div class="w-full" style:height={height + 'px'} />
