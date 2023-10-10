17eb368d12ae4c458cf66800a1684b6faa27c5ad
in choi <vcode@vcode-studio.com>
2023-10-10 13:45:09 +0900

    d

--- web/src/lib/components/PopUpBanner.svelte
+++ web/src/lib/components/PopUpBanner.svelte
@@ -25,6 +25,9 @@
     if (document.cookie.indexOf('popupYN=N') < 0) {
       //   enabled = true
     }
+    if (location.href.includes('?test')) {
+      enabled = true
+    }
   })
 </script>
 
@@ -33,9 +36,12 @@
     class="fixed left-4 z-[10000] bg-white border border-[navy] phone:left-[50%] phone:-translate-x-1/2 phone:w-[calc(100%-2rem)] top-1/2 max-w-[450px] -translate-y-1/2"
     id="popup"
   >
-    <a href={link} class="block" target="_blank">
-      <img class="block w-full h-auto" {src} alt="..." />
-    </a>
+    <div class="flex flex-col gap-2 p-4">
+      <p>2023년 10월 13일 금요일 11시부터 오송R&D센터 생중계를 아래의 링크에서 볼 수 있습니다.</p>
+      <a href={link} class="block" target="_blank">
+        <img class="block w-full h-auto" {src} alt="..." />
+      </a>
+    </div>
     <div class="flex gap-0 border-t border-[navy] h-[3em]">
       <a class="justify-center flex-1 button clean" href="#" on:click|preventDefault={setCookie}>
         <p class="w-full text-center">오늘하루 그만보기</p>
